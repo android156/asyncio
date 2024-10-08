e043da64524fb14007a38e98e662a9c3aa51235f
android156 <n_degtyarev@mail.ru>
2024-10-08 18:10:34 +0300

    new tasks added

---
+++ task22_get_running_loop.py
@@ -0,0 +1,31 @@
+import asyncio
+
+articles = [
+    {'title': 'Методы картирования генома', 'length': 3.2},
+    {'title': 'Гормоны растений и их рост', 'length': 4.5},
+    {'title': 'Применение CRISPR', 'length': 2.1},
+    {'title': 'Микробное разнообразие', 'length': 1.5},
+    {'title': 'Механика деления клеток', 'length': 4.1},
+    {'title': 'Эпигенетическая регуляция', 'length': 3.8},
+    {'title': 'Динамика сворачивания белков', 'length': 4.0},
+    {'title': 'Экологические взаимодействия', 'length': 0.7},
+    {'title': 'Модели нейронных сетей', 'length': 4.3},
+    {'title': 'Пути биолюминесценции', 'length': 2.9}
+]
+
+
+async def upload_article(article):
+    await asyncio.sleep(article['length'])
+    article['loop'] = asyncio.get_running_loop()
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    tasks = [loop.create_task(upload_article(article)) for article in articles]
+    await asyncio.gather(*tasks)
+
+    print(articles)
+
+
+asyncio.run(main())
